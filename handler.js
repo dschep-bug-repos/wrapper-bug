b845b016773efc59bf0008e5064f9ab46eff5136
Daniel Schep <dschep@gmail.com>
2019-08-01 10:22:29 -0400

    prettify

--- handler.js
+++ handler.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
 module.exports.hello = async event => {
   const returnObj = setTimeout(() => {
-    console.log('Timeout complete');
-  }, 60000)
+    console.log("Timeout complete");
+  }, 60000);
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: event
       },
       null,
       2
-    ),
+    )
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
--- wrapper.js
+++ wrapper.js
@@ -1,13 +1,13 @@
 const { hello } = require("./handler");
 const wrapper = (event, context, callback) => {
   context.done = (err, res) => {
-    callback(err, res)
+    callback(err, res);
   };
   context.succeed = res => {
-    callback(null, res)
+    callback(null, res);
   };
   context.fail = err => {
-    callback(err)
+    callback(err);
   };
 
   let result;
@@ -16,17 +16,17 @@ const wrapper = (event, context, callback) => {
   } catch (err) {
     callback(err);
   }
-  if (result && typeof result.then === 'function') {
+  if (result && typeof result.then === "function") {
     result
       .then(realRes => {
         callback(null, realRes);
       })
       .catch(err => {
         callback(err);
-      })
+      });
   } else {
-    return result
+    return result;
   }
-}
+};
 
-module.exports.wrapper = wrapper
+module.exports.wrapper = wrapper;
