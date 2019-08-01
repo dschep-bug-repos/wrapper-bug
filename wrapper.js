5a266dcc7d0a3e8ebfd91222ed12805f8e986c14
Daniel Schep <dschep@gmail.com>
2019-08-01 09:13:40 -0400

    :tada:

---
+++ wrapper.js
@@ -0,0 +1,32 @@
+const { hello } = require("./handler");
+const wrapper = (event, context, callback) => {
+  context.done = (err, res) => {
+    callback(err, res)
+  };
+  context.succeed = res => {
+    callback(null, res)
+  };
+  context.fail = err => {
+    callback(err)
+  };
+
+  let result;
+  try {
+    result = hello(event, context, callback);
+  } catch (err) {
+    callback(err);
+  }
+  if (result && typeof result.then === 'function') {
+    result
+      .then(realRes => {
+        callback(null, realRes);
+      })
+      .catch(err => {
+        callback(err);
+      })
+  } else {
+    return result
+  }
+}
+
+module.exports.wrapper = wrapper
